61546e025402856e37c5146f104f6fd296878674
pabgarcer <pabgarcer@alum.us.es>
2026-06-28 18:35:29 +0200

    Added logs

--- FifaLiga.jsx
+++ FifaLiga.jsx
@@ -2286,6 +2286,10 @@ export default function FifaLiga() {
   const CountryFlag = ({ country }) => {
     const code = getCountryCode(country);
 
+    console.log("RAW INPUT:", country);
+    console.log("RESOLVED CODE:", code);
+    console.log("FLAG EXISTS?:", Flags[code?.toUpperCase()]);
+
     if (!code) return <span>🌍</span>;
 
     const Key = code.toUpperCase();
